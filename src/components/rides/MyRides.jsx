16a520992c81f78fff33f9dd003d94ecc401f975
Caleb Ford <caleb.ford.sto@gmail.com>
2024-07-23 14:48:15 -0500

    search component for rides

--- src/components/rides/MyRides.jsx
+++ src/components/rides/MyRides.jsx
@@ -2,13 +2,23 @@ import { useEffect, useState } from "react";
 import { getRidesByUserId } from "../../services/rideService";
 import { Ride } from "./Ride";
 import "./Ride.css";
+import { RideFilterBar } from "../filter/RideFilterBar";
 
 export const MyRides = ({ currentUser }) => {
   const [currentUserRides, setCurrentUserRides] = useState([]);
   const [filteredRides, setFilteredRides] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
   const getAndSetCurrentUserRides = (userId) => {
     getRidesByUserId(userId).then((rides) => setCurrentUserRides(rides));
+  };
+  const filterRidesBySearchInput = () => {
+    if (searchInput) {
+      const searchedRides = currentUserRides.filter((ride) =>
+        ride.coaster.name.toLowerCase().includes(searchInput)
+      );
+      setFilteredRides(searchedRides);
+    }
   };
 
   useEffect(() => {
@@ -18,11 +28,13 @@ export const MyRides = ({ currentUser }) => {
   useEffect(() => {
     setFilteredRides(currentUserRides);
   }, [currentUserRides]);
-
+  useEffect(() => {
+    filterRidesBySearchInput(searchInput);
+  }, [searchInput]);
   return (
     <>
       <div className="filter-bar">
-        <h4>FilterStuff!</h4>
+        <RideFilterBar setSearchInput={setSearchInput} />
       </div>
       <div className="ride-container">
         {filteredRides.map((ride) => {
