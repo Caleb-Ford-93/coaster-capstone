a52fb47c309cbc3c3748522a18022a51b777d308
Caleb Ford <caleb.ford.sto@gmail.com>
2024-08-12 10:39:23 -0500

    small changes

--- src/components/users/UserProfile.jsx
+++ src/components/users/UserProfile.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { UserInfoCard } from "./UserInfoCard.jsx";
 import { useEffect, useState } from "react";
 import { getUserById } from "../../services/userService.jsx";
+import "./User.css";
+import { Container } from "react-bootstrap";
 
 export const UserProfile = () => {
   const [user, setUser] = useState({});
@@ -19,10 +21,12 @@ export const UserProfile = () => {
     getAndSetUser();
   }, []);
   return (
-    <UserInfoCard
-      user={user}
-      userStats={userStats}
-      setUserStats={setUserStats}
-    />
+    <Container>
+      <UserInfoCard
+        user={user}
+        userStats={userStats}
+        setUserStats={setUserStats}
+      />
+    </Container>
   );
 };
